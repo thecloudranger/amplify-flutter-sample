3199eb482eb014b7a7c484929baac145a5e8fa73
Qudrath <qudrat@3c22fba07b7d.ant.amazon.com>
2021-03-22 12:58:39 +0800

    Sample app after end of Module 1

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'sign_up_page.dart';
+import 'auth_service.dart';
+import 'verification_page.dart';
+import 'image_gallery_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// 1
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _authService = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+    _authService.showLogin();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Workshop App',
+      theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
+      // 2
+      home: StreamBuilder<AuthState>(
+          // 2
+          stream: _authService.authStateController.stream,
+          builder: (context, snapshot) {
+            // 3
+            if (snapshot.hasData) {
+              return Navigator(
+                pages: [
+                  // 4
+                  // Show Login Page
+                  if (snapshot.data.authFlowStatus == AuthFlowStatus.login)
+                    MaterialPage(
+                        child: LoginPage(
+                            didProvideCredentials:
+                                _authService.loginWithCredentials,
+                            shouldShowSignUp: _authService.showSignUp)),
+                  // 5
+                  // Show Sign Up Page
+                  if (snapshot.data.authFlowStatus == AuthFlowStatus.signUp)
+                    MaterialPage(
+                        child: SignUpPage(
+                            didProvideCredentials:
+                                _authService.signUpWithCredentials,
+                            shouldShowLogin: _authService.showLogin)),
+
+// Show Verification Code Page
+                  if (snapshot.data.authFlowStatus ==
+                      AuthFlowStatus.verification)
+                    MaterialPage(
+                        child: VerificationPage(
+                            didProvideVerificationCode:
+                                _authService.verifyCode)),
+                  if (snapshot.data.authFlowStatus == AuthFlowStatus.session)
+                    MaterialPage(
+                        child: GalleryView(shouldLogOut: _authService.logOut))
+                ],
+                onPopPage: (route, result) => route.didPop(result),
+              );
+            } else {
+              // 6
+              return Container(
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+    );
+  }
+}
